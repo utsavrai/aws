d912bf7b8ae85fdd606c53d51f886d8c6cf060cc
utsavrai <utsav.rai2015@vit.ac.in>
2018-02-19 14:53:27 +0000

    nfc2

--- nfc_src/testing.php
+++ nfc_src/testing.php
@@ -2,6 +2,10 @@
 $db_name = "nfc";
 $mysql_username = "root";
 $mysql_pwd = "root";
-$server_name = "18.219.103.88";
+$server_name = "127.0.0.1";
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_pwd, $db_name);
+if(!$conn){
+	die('could not connect'.mysqli_error());
+	}
+	echo 'Connected successfully';
 ?>
